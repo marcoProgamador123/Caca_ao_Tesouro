f8328a147a68c4faf9253df08b26cbcb1f212089
marcoProgamador123 <90981714+marcoProgamador123@users.noreply.github.com>
2022-02-10 08:55:52 -0300

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,22 @@
+const accessCode1 = "VARIÁVEL";
+const accessCode2 = "FUNÇÃO";
+const accessCode3 = "BANCO DE DADOS";
+
+function clues() {
+  fill("white");
+  textSize(15);
+  fill("lightblue");
+  text("Dica: Sempre mudando, não constante!", 100, 70);
+
+  // Adicione código para exibir as outras duas perguntas.
+  fill("white");
+  textSize(15);
+  fill("lightblue");
+  text("Dica: É tipo uma fabrica que cria qualquer coisa ", 650, 160);
+
+  fill("white");
+  textSize(15);
+  fill("lightblue");
+  text("Dica: Serve para fazer jogos online em tempo real ", 350,280 );
+  
+}
